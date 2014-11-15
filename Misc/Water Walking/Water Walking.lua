df690d16951eda3d609c5a00710fba3fed129f12
ImogenOC <heckoftoday@gmail.com>
2014-11-15 00:07:02 -0500

    Water Walking
    At the behest of Tao

---
+++ Misc/Water Walking/Water Walking.lua
@@ -0,0 +1,44 @@
+-- Constructed with love by ImogenOC. Shared and Used under the MIT Licensing.
+-- Should be in your OOC section.
+
+-- Water Walking (Shaman)
+{ "546", {
+	"!player.buff(546).duration <= 60", -- Waterwalking
+	"!player.buff(3714).duration <= 60", -- Path of Frost
+	"!player.buff(1706).duration <= 60", -- Levitate
+}},
+
+-- Water Walking Time Critical
+{ "1706", {
+	"!player.buff(546).duration <= 60", -- Waterwalking
+	"!player.buff(3714).duration <= 60", -- Path of Frost
+	"!player.buff(1706).duration <= 60", -- Levitate
+}},
+
+-- Path of Ice (Death Knight)
+{ "3714", {
+	"!player.buff(546).duration <= 60", -- Waterwalking
+	"!player.buff(3714).duration <= 60", -- Path of Frost
+	"!player.buff(1706).duration <= 60", -- Levitate
+}},
+
+-- Path of Ice (Death Knight) Time Critical
+{ "3714", {
+	"!player.buff(546).duration <= 60", -- Waterwalking
+	"!player.buff(3714).duration <= 60", -- Path of Frost
+	"!player.buff(1706).duration <= 60", -- Levitate
+}},
+
+-- Levitate (Priest)
+{ "1706", {
+	"!player.buff(546).duration <= 60", -- Waterwalking
+	"!player.buff(3714).duration <= 60", -- Path of Frost
+	"!player.buff(1706).duration <= 60", -- Levitate
+}},
+
+-- Levitate (Priest)
+{ "1706", {
+	"!player.buff(546).duration <= 60", -- Waterwalking
+	"!player.buff(3714).duration <= 60", -- Path of Frost
+	"!player.buff(1706).duration <= 60", -- Levitate
+}},
